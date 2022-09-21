d99323c74536b91f33376e281d421d2844690226
Marticvet <martigiant@gmail.com>
2022-09-21 22:45:50 +0200

    added headers

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ const app = express();
 
 // CORS Policy and Body Parser Third-party Middleware
 // app.use(cors(), bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
 app.use("/tires/manufacturers", tiresController);
 app.use("/sizes", sizesController);
 app.use("/searchBy", searchController);
-app.use('/users', usersController);
+app.use("/users", usersController);
 
 app.use((err, req, res, next) => {
     res.status(500).send({
