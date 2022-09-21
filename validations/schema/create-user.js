9e889fe68ff1cedc4120868ee2afce0f8a8cd482
Marticvet <martigiant@gmail.com>
2022-09-22 00:14:45 +0200

    return it how it was before

--- validations/schema/create-user.js
+++ validations/schema/create-user.js
@@ -1,44 +1,44 @@
 export const createUserSchema = {
     email: value => {
         if(!value){
-            return {message: 'Email is required'};
+            return 'Email is required';
         }
 
         if(typeof value !== 'string' || !/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(value)){
-            return {message: 'Email shoud be valid adress'};
+            return 'Email shoud be valid adress';
         } 
 
         return null;
     },
     password: value => {
         if(!value) {
-            return {message: 'Password is required'};
+            return 'Password is required';
         }
 
         if (typeof value !== 'string' || value.length < 8 || value.length > 25) {
-            return {message: 'Password should be a string in range [8..25]'};
+            return 'Password should be a string in range [8..25]';
         }
 
         return null;
     },
     first_name: value => {
         if(!value) {
-            return {message: 'First name is required'};
+            return 'First name is required';
         }
 
         if (typeof value !== 'string' || value.length < 1 || value.length > 25) {
-            return {message: 'First name should be a string in range [1..25]'};
+            return 'First name should be a string in range [1..25]';
         }
 
         return null;
     },
     last_name: value => {
         if(!value) {
-            return {message: 'Last name is required'};
+            return 'Last name is required';
         }
 
         if (typeof value !== 'string' || value.length < 1 || value.length > 25) {
-            return {message: 'Last name should be a string in range [1..25]'};
+            return 'Last name should be a string in range [1..25]';
         }
 
         return null;
