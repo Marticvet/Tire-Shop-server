e8212785c5c73f257864f3b0a57ff0bc0fc8fc72
Marticvet <martigiant@gmail.com>
2022-09-21 16:19:52 +0200

    deleted headers

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const app = express();
 app.use(cors(), bodyParser.json());
 app.use(passport.initialize());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 app.use("/tires/manufacturers", tiresController);
 app.use("/sizes", sizesController);
 app.use("/searchBy", searchController);
